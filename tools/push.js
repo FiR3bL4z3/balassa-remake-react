98626bef60f222657681bd0f65a2d0eb84d4a850
FiR3bL4z3 <horvathb0222@gmail.com>
2022-05-25 03:21:49 +0200

    Update push.js(msgs)

--- tools/push.js
+++ tools/push.js
@@ -1,12 +1,17 @@
 const { exec } = require("shelljs");
 const { argv } = require("process");
 const readlineSync = require("readline-sync");
+const { startmsg, infomsg, successmsg, endmsg } = require("./tools.config.js");
 
+console.log(startmsg("tools/push.js"));
 let commitMessage;
 if (argv.length <= 2) {
-  commitMessage = readlineSync.question("Commit message: ");
+  commitMessage = readlineSync.question(infomsg("Commit message: "));
 }
 
 exec("git add .");
 exec(`git commit -m "${commitMessage}"`);
 exec("git push");
+
+console.log(successmsg("Chaneges successfully commited!"));
+console.log(endmsg("tools/push.js"));
